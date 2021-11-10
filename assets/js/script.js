f5e426e04f94c2ee90726d17244b63ef2c58c1ce
rokamba <rokamba92@gmail.com>
2021-11-09 22:11:05 -0600

    hangle form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEL = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() { 
+
+
+
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+    
+  
     var listItemEl = document.createElement("li");
-    listItemEl.className ="task-item";
-    listItemEl.textContent = "this is a new task.";
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
     tasksToDoEL.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
+
